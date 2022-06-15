3817d1f21121d8825d025fdadbdde526d4706fae
omar1a4 <103324477+omar1a4@users.noreply.github.com>
2022-06-15 17:15:21 +0300

    Update Vampire.h

--- Cards/Vampire.h
+++ Cards/Vampire.h
@@ -1,13 +1,27 @@
 //
-// Created by user on 6/14/2022.
+// Created by Omar on 15/06/2022.
 //
 
 #ifndef EX4_VAMPIRE_H
 #define EX4_VAMPIRE_H
+#include "Battle.h"
 
+class Vampire : public Battle
+{
+public:
+    Vampire(): Battle(10,2){};
+    Vampire(const Vampire& otherVampire)=default;
+    Vampire& operator=(const Vampire& otherVampire)=default;
+    ~Vampire()=default;
+    Card* clone() override;
 
-class Vampire {
+protected:
+    void print(std::ostream& os) const override;
+    void onWinBattle(Player& player) override;
+    void onLossBattle(Player& player)override;
 
+private:
+    static const int m_hpDamage = -10;
 };
 
 
