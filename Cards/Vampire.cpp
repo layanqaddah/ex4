b17c88a72987ea6689982c5106c6f4aecfcb937b
omar1a4 <103324477+omar1a4@users.noreply.github.com>
2022-06-15 17:15:56 +0300

    Update Vampire.cpp

--- Cards/Vampire.cpp
+++ Cards/Vampire.cpp
@@ -1,5 +1,27 @@
 //
-// Created by user on 6/14/2022.
+// Created by Omar on 6/14/2022.
 //
 
 #include "Vampire.h"
+
+Card* Vampire::clone()
+{
+    return new Vampire(*this);
+}
+void Vampire::print(std::ostream& os)const
+{
+    printCardDetails(os,"Vampire");
+    printMonsterDetails(os,m_force,-m_hpDamage,m_loot);
+    printEndOfCardDetails(os);
+}
+void Vampire::onWinBattle(Player& player)
+{
+    player.addCoins(m_loot);
+    printWinBattle(player.getPlayerName(),"Vampire");
+    player.levelUp();
+}
+void Vampire::onLossBattle(Player& player)
+{
+    player.setHp(m_hpDamage);
+    printLossBattle(player.getPlayerName(),"Vampire");
+}
