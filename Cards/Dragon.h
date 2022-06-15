eb3e222544626abf1f6ff2ed8a05c16bfaf6e175
layanqaddah <103147665+layanqaddah@users.noreply.github.com>
2022-06-15 17:11:01 +0300

    Update Dragon.h

--- Cards/Dragon.h
+++ Cards/Dragon.h
@@ -1,12 +1,29 @@
-//
-// Created by Layan on 6/14/2022.
+ //
+// Created by user on 6/14/2022.
 //
 
 #ifndef EX4_DRAGON_H
 #define EX4_DRAGON_H
 
+#include "Battle.h"
 
-class Dragon {
+
+class Dragon :public Battle{
+
+public:
+
+    Dragon(): Battle(25,1000){};
+    Dragon(const Dragon& otherWizard)=default;
+    Dragon& operator=(const Dragon& otherWizard)=default;
+    ~Dragon()=default;
+    Card* clone() override;
+
+protected:
+        void print(std::ostream& os) const override;
+        void onWinBattle(Player& player) override;
+        void onLossBattle(Player& player)override;
+private:
+
 
 };
 
