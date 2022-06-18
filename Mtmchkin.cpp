21b017dd8f1d0c9e0a73c3125804abc90e46c666
omar1a4 <103324477+omar1a4@users.noreply.github.com>
2022-06-18 15:31:54 +0300

    Add files via upload

---
+++ Mtmchkin.cpp
@@ -0,0 +1,11 @@
+//
+// Created by assur on 16/06/2022.
+//
+
+#include "Mtmchkin.h"
+#include "Exception.h"
+
+const std::string DeckFileException::BASE_MESSAGE = "Deck File Error: ";
+const std::string DeckFileNotFound::FILE_NOT_FOUND_ERROR = "File not found";
+const std::string DeckFileFormatError::FILE_FORMAT_ERROR = "File format error in line ";
+const std::string DeckFileInvalidSize::INVALID_SIZE_ERROR = "Deck size is invalid";
