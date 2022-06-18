0d3aa92c13665b54c261530997a846072e1162d8
omar1a4 <103324477+omar1a4@users.noreply.github.com>
2022-06-18 15:31:03 +0300

    Update Exception.h

--- Exception.h
+++ Exception.h
@@ -1,8 +1,51 @@
 //
-// Created by Layan & Omar on 6/13/2022.
+// Created by Omar on 18/06/2022.
 //
 
 #ifndef EX4_EXCEPTION_H
 #define EX4_EXCEPTION_H
+
+
+//Error messages are initialized in Mtmchkin.cpp
+
+class DeckFileException : public std::runtime_error
+{
+public:
+    DeckFileException(const std::string& what): std::runtime_error(what){};
+protected:
+    static const std::string BASE_MESSAGE;
+};
+
+
+class DeckFileNotFound : public DeckFileException
+{
+public:
+    DeckFileNotFound():
+                     DeckFileException(BASE_MESSAGE + FILE_NOT_FOUND_ERROR){};
+private:
+    static const std::string FILE_NOT_FOUND_ERROR;
+};
+
+
+class DeckFileFormatError : public DeckFileException
+{
+public:
+    DeckFileFormatError(const std::string& lineNumber):
+                        DeckFileException(BASE_MESSAGE+FILE_FORMAT_ERROR+lineNumber){};
+private:
+    static const std::string FILE_FORMAT_ERROR;
+};
+
+
+class DeckFileInvalidSize : public DeckFileException
+{
+public:
+    DeckFileInvalidSize():
+                        DeckFileException(BASE_MESSAGE+INVALID_SIZE_ERROR){};
+private:
+    static const std::string INVALID_SIZE_ERROR;
+};
+
+
 
 #endif //EX4_EXCEPTION_H
